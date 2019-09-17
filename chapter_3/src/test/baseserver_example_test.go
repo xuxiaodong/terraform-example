2792ef684eab27dbfc5e7df1784c116d3180843d
Xiaodong Xu <xxdlhy@gmail.com>
2019-09-17 10:27:15 +0800

    update test

--- chapter_3/src/test/baseserver_example_test.go
+++ chapter_3/src/test/baseserver_example_test.go
@@ -1,32 +1,46 @@
 package test
 
 import (
+	"crypto/tls"
 	"fmt"
+	"testing"
 	"time"
 	"github.com/gruntwork-io/terratest/modules/http-helper"
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	"testing"
 )
 
 func TestBaseserverExample(t *testing.T)  {
+	// 配置 Terraform
 	opts := &terraform.Options{
 		TerraformDir: "../",
 	}
 
+	// 测试完成后，执行 `terraform destroy` 清理资源
+	defer terraform.Destroy(t, terraformOptions)
+
+	// 初始化并运行 Terraform 代码
 	terraform.InitAndApply(t, opts)
 
+	// 执行 `terraform output` 以获取输出变量的值
 	serverIp := terraform.OutputRequired(t, opts, "ip")
 	url := fmt.Sprintf("http://%s", serverIp)
 
+	// 设置 TLS，http_helper 需要
+	tlsConfig := tls.Config{}
+
+	// 期望得到的 HTTP 状态及消息
 	expectedStatus := 200
 	expectedBody := "Hello, 2019"
 
+	// 设置重试次数
 	maxRetries := 10
 	timeBetweenRetries := 10 * time.Second
 
+	// 验证 HTTP
 	http_helper.HttpGetWithRetry(
 		t,
 		url,
+		&tlsConfig,
 		expectedStatus,
 		expectedBody,
 		maxRetries,
